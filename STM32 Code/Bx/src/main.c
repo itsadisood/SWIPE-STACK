5b59a429177ae717c5f3b665080c05d626091d7c
Aditya Sood <sood0@outlook.com>
2024-01-18 21:13:19 -0500

    bx working

--- STM32 Code/Bx/src/main.c
+++ STM32 Code/Bx/src/main.c
@@ -15,30 +15,59 @@
 int main(void)
 {
 
-	for(;;);
+//	for(;;);
 
 	setupGPIO(); // Pa0, Pa1 key
 	setupUART(); // utilizing usart4 to communicate with Bx
-	sendATCheck();
+//	sendATCheck(); // check handshake with OK reception
+	sendATAddr(); // send at command to get address
+}
+
+void sendATAddr()
+{
+	  uint8_t* testAddrTx[8] = {'A','T','+','A','D','D','R','?'};
+	  uint8_t* testAddrRx[20] = {};
+
+	  // send "AT" test command
+	  for(uint32_t i = 0; i < 8; i++){
+		  while(!(USART4->ISR & USART_ISR_TXE)){}
+		  USART4 -> TDR = testAddrTx[i];
+	  }
+
+	  // expect "OK" from Bx
+	  for(int i = 0; i < 20; i++)
+	  {
+		  while (!(USART4->ISR & USART_ISR_RXNE)) {}
+		  testAddrRx[i] = USART4->RDR;
+	  }
+	  return 0;
 }
 
 void sendATCheck()
 {
-	  char* testTX[2] = {'A','T'};
-	  char* testRX[2] = {};
+	  uint8_t* testTX[2] = {'A','T'};
+	  uint8_t* testRX[2] = {};
 
 	  // send "AT" test command
-	  for(uint32_t i = 0; i < 2;i++){
+	  for(uint32_t i = 0; i < 2; i++){
 		  while(!(USART4->ISR & USART_ISR_TXE)){}
 		  USART4 -> TDR = testTX[i];
 	  }
+
 	  // expect "OK" from Bx
 	  for(int i = 0; i < 2; i++)
 	  {
 		  while (!(USART4->ISR & USART_ISR_RXNE)) {}
 		  testRX[i] = USART4->RDR;
 	  }
+
+	  if(strcmp(testRX,"OK"))
+	  {
+		  return 1;
+	  }
+	  return 0;
 }
+
 void setupGPIO(){
 	RCC -> AHBENR |= RCC_AHBENR_GPIOAEN;                    // Enable GPIO clock
 	GPIOA -> MODER |= GPIO_MODER_MODER0_1; 					// PA0 is TX to Bluetooth (Alt Mode)
