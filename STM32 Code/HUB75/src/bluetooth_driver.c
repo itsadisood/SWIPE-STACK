ed434b5b14136feee8ab26d26719989125735c13
lagudu-glitch <laguduvishnu@gmail.com>
2024-04-05 23:40:25 -0400

    finish the pheriphals for bt

--- STM32 Code/HUB75/src/bluetooth_driver.c
+++ STM32 Code/HUB75/src/bluetooth_driver.c
@@ -6,6 +6,8 @@
  *         Aditya Sood (sood12@purdue.edu)
 **/
 
+char MOVE;
+
 #include "bluetooth_driver.h"
 
 /* Coinfigure the peripherals required for UART transmission */
@@ -23,33 +25,33 @@ init_io_bt ()
 void 
 setup_uart_bt ()
 {
-  RCC -> APB2ENR |= RCC_APB2ENR_USART7EN;   
+  RCC -> APB1ENR |= RCC_APB1ENR_USART4EN;   
   // Set the control register reset state             
-	USART7 -> CR1 = 0;                                            
-	USART7 -> BRR = 48000000 / 9600;					  
-	USART7 -> CR1 |= USART_CR1_TE;				           
-	USART7 -> CR1 |= USART_CR1_RE;				           
-	USART7 -> CR1 |= USART_CR1_UE;                         
+	USART4 -> CR1 = 0;                                            
+	USART4 -> BRR = 48000000 / 9600;					  
+	USART4 -> CR1 |= USART_CR1_TE;				           
+	USART4 -> CR1 |= USART_CR1_RE;				           
+	USART4 -> CR1 |= USART_CR1_UE;   
+	USART4 -> CR1 |= USART_CR1_RXNEIE;
+	USART4 -> CR3 |= USART_CR3_DMAR;               
   // wait for TX and RX to be ready
-	while(!((USART5 -> ISR) & (USART_ISR_TEACK | USART_ISR_REACK)));
+	while(!((USART4 -> ISR) & (USART_ISR_TEACK | USART_ISR_REACK)));
 }
 
 // need to fix this
 void
 init_dma_bt ()
 {
-  RCC->AHBENR |= RCC_AHBENR_DMA1EN;
-	DMA1_Channel3->CCR &= ~DMA_CCR_EN;      			
-	DMA1_Channel3->CMAR = (uint32_t) data_fifo;    	 	
-	DMA1_Channel3->CPAR = (uint32_t) &(USART1->RDR);
-	DMA1_Channel3->CNDTR = FIFOSIZE;        			
-	DMA1_Channel3->CCR &= ~DMA_CCR_DIR;     			
-	DMA1_Channel3->CCR &= ~DMA_CCR_MSIZE;   			
-	DMA1_Channel3->CCR &= ~DMA_CCR_PSIZE;   			
-	DMA1_Channel3->CCR |= DMA_CCR_MINC;     			
-	DMA1_Channel3->CCR |= DMA_CCR_CIRC;					
-	DMA1_Channel3->CCR |= DMA_CCR_PL_0 | DMA_CCR_PL_1;  
-	DMA1_Channel3->CCR |= DMA_CCR_TCIE; 				
-	DMA1_Channel3->CCR |= DMA_CCR_EN;	
-	NVIC -> ISER[0] |= (1 << DMA1_Ch2_3_DMA2_Ch1_2_IRQn);
+  DMA1_Channel6 -> CCR &= ~DMA_CCR_EN;
+	DMA1_Channel6 -> CMAR = &MOVE;
+	DMA1 -> RMPCR |= DMA_RMPCR2_CH2_USART4_RX;
+	DMA1_Channel6 -> CPAR = (uint32_t) &(USART4->RDR);
+	DMA1_Channel6 -> CNDTR = sizeof (char);
+	DMA1_Channel6 -> CCR |= DMA_CCR_PL;
+}
+
+void
+enable_dma_bt ()
+{
+	DMA1_Channel6 -> CCR |= DMA_CCR_EN;
 }
