6a846deba67fdc7baba1adb98611dc4f94a2abfa
lagudu-glitch <laguduvishnu@gmail.com>
2024-04-05 20:12:27 -0400

    Fixed ?

---
+++ STM32 Code/Glove_Code/Debug/StdPeriph_Driver/src/stm32f0xx_flash.d
@@ -0,0 +1,92 @@
+StdPeriph_Driver/src/stm32f0xx_flash.o: \
+ ../StdPeriph_Driver/src/stm32f0xx_flash.c \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_flash.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/device/stm32f0xx.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/core/core_cm0.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/core/core_cmInstr.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/core/core_cmFunc.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/device/system_stm32f0xx.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_conf.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_adc.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_can.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_cec.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_crc.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_crs.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_comp.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_dac.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_dma.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_exti.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_flash.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_gpio.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_syscfg.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_i2c.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_iwdg.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_pwr.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_rcc.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_rtc.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_spi.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_tim.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_usart.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_wwdg.h \
+ /Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_misc.h
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_flash.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/device/stm32f0xx.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/core/core_cm0.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/core/core_cmInstr.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/core/core_cmFunc.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/CMSIS/device/system_stm32f0xx.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_conf.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_adc.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_can.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_cec.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_crc.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_crs.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_comp.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_dac.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_dbgmcu.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_dma.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_exti.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_flash.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_gpio.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_syscfg.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_i2c.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_iwdg.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_pwr.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_rcc.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_rtc.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_spi.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_tim.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_usart.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_wwdg.h:
+
+/Users/vishnulagudu/Documents/projects/SWIPE-STACK/STM32\ Code/Glove_Code/StdPeriph_Driver/inc/stm32f0xx_misc.h:
